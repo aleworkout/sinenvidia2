bb3c8a36af73108cc82e2cf1b2bce6a47b14da2f
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2017-11-14 01:34:34 +0000

    commit

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,1 +1,11 @@
-Refile.backends['store'] = Refile::Backend::FileSystem.new(Rails.root.join('tmp').to_s)
+if Rails.env.production?
+  aws = {
+    access_key_id: "AKIAJGRXQD2TKYEQOR3A", 
+    secret_access_key: "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", 
+    bucket: "bucketaleworkout",
+  } 
+  Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
+  else
+      Refile.backends['store'] = Refile::Backend::FileSystem.new(Rails.root.join('tmp').to_s)
+end
